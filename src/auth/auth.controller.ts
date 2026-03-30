35dd6771a5d0a72e9b3b9e20929890a9380ca09d
Jhony <jhony.duque@correounivalle.edu.co>
2026-03-30 11:54:56 -0500

    Sanitización de tipos y configuración de JWT Secret en .env

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,36 +8,39 @@ import {
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import type { Response } from 'express';
+import { ApiOperation, ApiProperty, ApiResponse } from '@nestjs/swagger';
+import { LoginDto } from './dto/loginDto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService) { }
 
   @Post('login')
+  @ApiOperation({ summary: 'Login del usuario', description: 'Devuelve el JWT del usuario a través de las cookies.' })
+  @ApiResponse({ status: 200, description: 'Entre socio, por la sombrita.' })
+  @ApiResponse({ status: 401, description: 'Credenciales incorrectas, ¿Quién sos vos?' })
   @HttpCode(200)
   async login(
-    @Body() body: { email: string; password: string },
+    @Body() body: LoginDto,
     @Res({ passthrough: true }) res: Response,
   ) {
     const user = await this.authService.validateUser(body.email, body.password);
 
     if (!user) {
-      throw new UnauthorizedException('Credenciales incorrectas ¿Quién sos vos?',);
+      throw new UnauthorizedException('Credenciales incorrectas ¿Quién sos vos?');
     }
 
-    const { access_token } = await this.authService.login({
-      email: body.email,
-      password: body.password,
-    });
+    const { access_token } = await this.authService.login(body);
 
     res.cookie('token', access_token, {
       httpOnly: true,
-      secure: false,
+      secure: process.env.NODE_ENV === 'production',
       sameSite: 'lax',
       maxAge: 1000 * 60 * 60,
     });
 
     return {
-      message: 'Entre socio, por la sombrita',};
+      message: 'Entre socio, por la sombrita',
+    };
   }
 }

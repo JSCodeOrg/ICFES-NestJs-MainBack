a237cdd607697eef6e8a0fa940f1e418434b5581
StivenRuiz2005 <carlosruizrojas2005@gmail.com>
2026-04-05 19:03:34 -0500

    Endpoints de Promedio nacional, total de registros y comparacion de colegios

--- src/icfes/dto/promedioAnualDto.ts
+++ src/icfes/dto/promedioAnualDto.ts
@@ -1,10 +1,10 @@
-import { ApiProperty } from "@nestjs/swagger";
-import { Type } from "class-transformer";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import { IsNumber } from 'class-validator';
 
 export class PromedioAnualDto {
-    @ApiProperty({ description: 'Año de solicitud de promedio', example: '2018' })
-    @Type(() => Number)
-    @IsNumber()
-    ano: number;
+  @ApiProperty({ description: 'Año de solicitud de promedio', example: '2018' })
+  @Type(() => Number)
+  @IsNumber()
+  ano: number;
 }
